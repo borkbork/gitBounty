a8e1975348f7da123099718fdeb87f761c2426e2
oskar367 <oskar.jedvert@gmail.com>
2016-05-14 16:19:56 +0200

    adds caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,21 +14,27 @@ module ApplicationHelper
   end
 
   def get_dev_level
-    get_closed_issues.size * 10/2
+    Rails.cache.fetch('dev_level/#{github_user.api.user.name}') do
+      xp = 0
+      get_levels.each do |lang, val| xp += val end
+      xp
+    end
   end
 
-  def get_xp
-    issues = get_closed_issues
-    xp = Hash.new
-    issues.each do |issue|
-      langs = github_user.api.languages(issue.repository.full_name)
-      points = (issue.closed_at - issue.created_at)/(24 * 60 * 60).to_f/3
-      total_rows = 0
-      langs.each do |lang, val| total_rows += val end
-      langs.each do |lang, val|
-        xp[lang] = (val.to_f/total_rows.to_f)*points
+  def get_levels
+    Rails.cache.fetch('xp/#{github_user.api.user.name}') do
+      issues = get_closed_issues
+      xp = Hash.new
+      issues.each do |issue|
+        langs = github_user.api.languages(issue.repository.full_name)
+        points = (issue.closed_at - issue.created_at)/(24 * 60 * 60).to_f/3
+        total_rows = 0
+        langs.each do |lang, val| total_rows += val end
+        langs.each do |lang, val|
+          xp[lang] = (val.to_f/total_rows.to_f)*points
+        end
       end
+      xp
     end
-    xp
   end
 end
