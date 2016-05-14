dab5a79606fd22edc38033b59854831e26b80870
Tobias <covernant_4@hotmail.com>
2016-05-14 11:43:11 +0200

    Added controller, fixed routes and db

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get '/user/:id' => 'issuecontroller#index', as: :id
+
+
 end
--- db/migrate/20160514085211_create_issues.rb
+++ db/migrate/20160514085211_create_issues.rb
@@ -1,8 +1,8 @@
 class CreateIssues < ActiveRecord::Migration
   def change
-    create_table :|
-      t.string user
-      t.string repo 
+    create_table :issues do |t|
+      t.string :user
+      t.string :repo
       t.timestamps null: false
     end
   end
