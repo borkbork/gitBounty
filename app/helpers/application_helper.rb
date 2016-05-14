7718e42fc44bccb1f700ff8377ff92950a52dc0b
oskar367 <oskar.jedvert@gmail.com>
2016-05-14 12:46:48 +0200

    fixed issue where user where unable to logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
   def current_user_local
-	User.where(:git_id => github_user.api.user.id).first
+	   User.where(:git_id => current_user.id).first
   end
 
   def current_user
-	if github_user
-	  github_user.api.user
-	else
-	  nil
-	end
+  	if github_authenticated?
+  	  @user = github_user.api.user
+  	end
+    @user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514074907) do
-
-  create_table "users", force: :cascade do |t|
-    t.integer  "git_id"
-    t.integer  "xp"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
-
 ActiveRecord::Schema.define(version: 20160514085211) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "user"
     t.string   "repo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "git_id"
+    t.integer  "xp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
